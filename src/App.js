9e204b4d27272984bb8ca9ecbb55528109593b8f
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-05-05 11:27:57 -0400

    iteration 3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import "./App.css";
 import contactsJson from "./contacts.json";
 
 let firstFive = contactsJson.slice(0, 5);
+let theRest = contactsJson.slice(5);
+
 function App() {
   const [contacts, setContacts] = React.useState(firstFive);
+  const [remainingCelebs, setRemainingCelebs] = React.useState(theRest);
+
+  function addContact() {
+    if (remainingCelebs.length > 0) {
+      let randomNum = Math.floor(Math.random() * remainingCelebs.length);
+      //This pulls a random celebrity from remainingCelebs
+      let randomCeleb = remainingCelebs[randomNum];
+
+      //add a new celeb to our contacts list
+      setContacts(contacts.concat(randomCeleb));
+
+      //remove that celebrity from the non-used array
+      let filteredArr = remainingCelebs.filter((celeb) => {
+        return celeb.id !== randomCeleb.id;
+      });
+
+      //set the remaining celebs
+      setRemainingCelebs(filteredArr);
+    }
+  }
 
   return (
     <div>
+      <button onClick={addContact}>Add Random Contact</button>
       <table>
         <tr>
           <th>picture</th>
